2a5db8eed901aff748e28dd6b8e0cc328b3ce391
Samuel Sulo <samuelsulo0802@gmail.com>
2022-05-15 10:18:49 +0200

    feat(scripts): add create-components script

---
+++ scripts/create-components.sh
@@ -0,0 +1,27 @@
+rel_path=$1
+components=$2
+
+cd $rel_path
+
+echo $components | tr "," "\n" | while read line;
+do
+  mkdir $line || continue
+  cd $line 
+  component_name=$(echo $line | cut -c 1 | tr [:lower:] [:upper:])$(echo $line | cut -c 2-)
+  style=$line
+  touch $component_name.jsx $style.css
+
+  echo "import React from 'react'
+import './$style.css';
+
+const $component_name = () => {
+  return (
+    <div>$component_name</div>
+  )
+}
+
+export default $component_name" >> $component_name.jsx
+
+  cd ..
+  echo "export { default as $component_name } from './$line/$component_name'" >> index.js;
+done
--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Footer, Blog, Features, Header, Possibility, WhatGPT3 } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
   )
 }
 
