5e1b7d39935b8f08e5152b16b6b6c825d2d2971c
Samuel Sulo <81391401+Samuel-Sulo@users.noreply.github.com>
2022-05-16 20:13:38 +0200

    Feature/future is now (#6)
    * feat(general): create folder structure
    
    * feat(scripts): add create-components script
    
    * feat(general): add global css classes
    
    * Feature/navbar (#1)
    
    * feat(navbar): add responsive navbar
    
    * feat(assets): add assets
    
    * feat(eslint): add eslint configuration (#2)
    
    * feat(header): add header component
    
    * feat(header): create component, start styling
    
    * feat(eslint): add missing semicolumns
    
    * feat(header): make header component responsive
    
    * feat(brand): add brand component (#4)
    
    * feat(whatgpt3): add whatgpt3 component (#5)
    
    * fix(eslint): turn off rule
    
    * feat(features): add features component

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+/* eslint-env node */
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'react/prop-types': ['off']
+  }
+};
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # modern-ui-ux-website
  Fully Responsive Modern UI/UX Website in React JS
+
+# create containers or components
+bash ./scripts/create-components.sh <relative_path> <list_of_components>
+
+## examples:
+bash ./scripts/create-components.sh src/containers blog,features,header,footer
+bash ./scripts/create-components.sh src/components button,input,spinner
